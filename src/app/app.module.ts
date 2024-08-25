2a9580d536628e602511c32dfc85ec242c73ba89
Shivam Nagpal <shivamnagpal432@gmail.com>
2024-08-25 21:56:54 +0530

    feature: #11 | Wire Navigation Bar and initial layout styling and responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NavBarComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
