54ad4fa772d6c515ba15bd38258a7fa9171f19af
Shivam Nagpal <shivamnagpal432@gmail.com>
2026-01-10 16:46:46 +0530

    feature: #28 | Build Video Tab component

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+class VideoItem {
+  title: string;
+  id: string;
+
+  constructor(title: string, id: string) {
+    this.title = title;
+    this.id = id;
+  }
+
+  track = () => {
+    return this.id;
+  };
+}
 
 @Component({
   selector: 'app-videos',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './videos.component.html',
   styleUrl: './videos.component.scss',
 })
-export class VideosComponent {}
+export class VideosComponent {
+  videoItems: VideoItem[] = [
+    new VideoItem(
+      'Deploying Multiple Vert.x Verticles with Spring Boot',
+      '2SlRRzFaVfA',
+    ),
+    new VideoItem('Vert.x Integration with Spring Boot', 'T31KQVPnCLo'),
+  ];
+}
